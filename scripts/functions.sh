8a8b00cdb661c2539303ef0072eed4ef708ef233
Agnete Moos <agms@sonderborg.dk>
2023-10-02 10:49:05 +0200

    Bugfix - Missing semicolon

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -395,7 +395,7 @@ printf "Copying nginx.conf.example to os2display-nginx.conf\n"
 sudo cp nginx.conf.example $NGINX_CONF_FILE	
 printf "Updating os2display-nginx.conf\n"
   
-sudo sed -i "/server_name /c\  server_name $DOMAIN" $NGINX_CONF_FILE
+sudo sed -i "/server_name /c\  server_name $DOMAIN;" $NGINX_CONF_FILE
 sudo sed -i "/ssl_certificate /c\  ssl_certificate /etc/ssl/$DOMAIN.pem;" $NGINX_CONF_FILE
 sudo sed -i "/ssl_certificate_key /c\  ssl_certificate_key /etc/ssl/$DOMAIN-key.pem;" $NGINX_CONF_FILE
 
